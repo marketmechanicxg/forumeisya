d7ea0f7f4739106182c7a29f90f622e0f981fc70
Grizzy <grizzyxixi@email.com>
2026-05-29 19:19:39 +0700

    update

--- js/audio-system.js
+++ js/audio-system.js
@@ -14,8 +14,8 @@
 const playlist = [
   {
     id:        'song1',
-    title:     'Risk It All',
-    artist:    'Bruno Mars',
+    title:     'Shape of My Heart',
+    artist:    'Backstreet Boys',
     file:      'music/song1.mp3',
     cover:     'assets/music-cover/song1.jpeg',
     startTime: 0,       // seconds — where playback begins
